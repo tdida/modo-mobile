aa99111922ab08761dd6566be0275e83c55a05b2
TDiDa <tdida@qq.com>
2018-08-28 01:40:31 +0800

    update import

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-/* eslint no-console:0 */
-function camelCase(name) {
-  return name.charAt(0).toUpperCase() +
-    name.slice(1).replace(/-(\w)/g, (m, n) => {
-      return n.toUpperCase();
-    });
-}
+/**
+ * This is the entry file for `npm run dist`
+ */
 
-// Just import style for https://github.com/ant-design/ant-design/issues/3745
+/**
+ * build for `dist/modo-mobile.css`
+ */
 const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
-
-req.keys().forEach((mod) => {
-  let v = req(mod);
-  if (v && v.default) {
-    v = v.default;
-  }
-  const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
-  if (match && match[1]) {
-    if (match[1] === 'message' || match[1] === 'notification') {
-      // message & notification should not be capitalized
-      exports[match[1]] = v;
-    } else {
-      exports[camelCase(match[1])] = v;
-    }
-  }
+req.keys().forEach(mod => {
+  req(mod);
 });
 
-module.exports = require('./components');
+/**
+ * build for `dist/modo-mobile.js`
+ */
+module.exports = require('./components/index');
