cf7dbab22e52f4d03f7a24ae3ec3a483d05fbb50
TDiDa <tdida@qq.com>
2018-08-28 00:45:05 +0800

    not gitignore components/**/__tests__/*.js and components/**/__tests__/*.js.snap

---
+++ components/__tests__/index.test.js
@@ -0,0 +1,21 @@
+const OLD_NODE_ENV = process.env.NODE_ENV;
+process.env.NODE_ENV = 'development';
+const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+const modo = require('..');
+
+describe('modo', () => {
+  afterAll(() => {
+    process.env.NODE_ENV = OLD_NODE_ENV;
+  });
+
+  it('exports modules correctly', () => {
+    expect(Object.keys(modo)).toMatchSnapshot();
+  });
+
+  it('should hint when import all components in dev mode', () => {
+    expect(warnSpy).toBeCalledWith(
+      'You are using a whole package of modo-mobile, please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.'
+    );
+    warnSpy.mockRestore();
+  });
+});
