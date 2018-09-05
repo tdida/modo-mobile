68d6d2ffed3d7e01c24002bbde7ceadd9186a76d
tdida <tdida@qq.com>
2018-09-05 13:17:37 +0800

    [chore] update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,5 @@
-const webpack = require('webpack');
-const WebpackBar = require('webpackbar');
 const getWebpackConfig = require('./tools/getWebpackConfig');
 
-function usePrettyWebpackBar(config) {
-  // remove old progress plugin.
-  config.plugins = config.plugins.filter(
-    plugin => !(plugin instanceof webpack.ProgressPlugin) && !(plugin instanceof WebpackBar)
-  );
-
-  // use brand new progress bar.
-  config.plugins.push(
-    new WebpackBar({
-      name: '📦  Webpack',
-      minimal: false,
-    })
-  );
-}
-
 const webpackConfig = getWebpackConfig(false);
-if (process.env.RUN_ENV === 'PRODUCTION') {
-  webpackConfig.forEach(config => {
-    usePrettyWebpackBar(config);
-  });
-}
 
 module.exports = webpackConfig;
